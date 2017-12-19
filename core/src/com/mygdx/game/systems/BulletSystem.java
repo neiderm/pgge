ca4451ddd75232a264f1076627eaf872cba2503e
G. Neidermeier <ded.reckning@gmail.com>
2017-12-19 18:14:06 -0500

    BulletSystem implements EntityListener

---
+++ core/src/com/mygdx/game/systems/BulletSystem.java
@@ -0,0 +1,42 @@
+package com.mygdx.game.systems;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.EntityListener;
+import com.badlogic.ashley.core.EntitySystem;
+
+/**
+ * Created by mango on 12/18/17.
+ */
+
+public class BulletSystem extends EntitySystem implements EntityListener {
+
+    public BulletSystem() {
+
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void addedToEngine(Engine engine) {
+
+    }
+
+    @Override
+    public void removedFromEngine(Engine engine) {
+
+    }
+
+    @Override
+    public void entityAdded(Entity entity) {
+
+    }
+
+    @Override
+    public void entityRemoved(Entity entity) {
+
+    }
+}
