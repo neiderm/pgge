3809e27673a47f106136938064b8e8d06e51ba5a
GlennN <GlennN@anywhere.com>
2018-03-01 12:45:27 -0500

    Does not need to be EntityListener.

--- core/src/com/mygdx/game/systems/CharacterSystem.java
+++ core/src/com/mygdx/game/systems/CharacterSystem.java
@@ -2,37 +2,24 @@ package com.mygdx.game.systems;
 
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
-import com.badlogic.ashley.core.EntityListener;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.utils.ImmutableArray;
 import com.mygdx.game.Components.CharacterComponent;
-import com.mygdx.game.Components.ModelComponent;
 
 /**
  * Created by mango on 2/10/18.
  */
 
-public class CharacterSystem extends EntitySystem implements EntityListener {
+public class CharacterSystem extends EntitySystem {
 
     private ImmutableArray<Entity> entities;
-
-
-    @Override
-    public void entityAdded(Entity entity) {
-    }
-
-    @Override
-    public void entityRemoved(Entity entity) {
-    }
 
     @Override
     public void addedToEngine(Engine engine) {
 
         // Grabs all entities with desired components
         entities = engine.getEntitiesFor(Family.all(CharacterComponent.class).get());
-
-        engine.addEntityListener(Family.all(CharacterComponent.class).get(), this);
     }
 
     @Override
@@ -46,4 +33,3 @@ public class CharacterSystem extends EntitySystem implements EntityListener {
         }
     }
 }
-
