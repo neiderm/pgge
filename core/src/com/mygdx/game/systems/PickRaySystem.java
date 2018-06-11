ccb6a0559a908d94133ea4dd7651241dc9f48c38
GlennN <GlennN@anywhere.com>
2018-06-11 13:24:58 -0400

    First pass at cleanup/refactor.

--- core/src/com/mygdx/game/systems/PickRaySystem.java
+++ core/src/com/mygdx/game/systems/PickRaySystem.java
@@ -6,7 +6,6 @@ import com.badlogic.ashley.core.EntityListener;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
@@ -40,6 +39,7 @@ public class PickRaySystem extends IteratingSystem implements EntityListener {
 
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
+        // empty
     }
 
     @Override
@@ -66,44 +66,11 @@ public class PickRaySystem extends IteratingSystem implements EntityListener {
 
         for (Entity e : getEntities()) {
 
-            ModelComponent mc = e.getComponent(ModelComponent.class);
-
-            mc.modelInst.transform.getTranslation(position).add(mc.center);
-
-            if (false) {
-                float dist2 = ray.origin.dst2(position);
-
-                if (distance >= 0f && dist2 > distance)
-                    continue;
-
-                if (Intersector.intersectRaySphere(ray, position, mc.boundingRadius, null)) {
-                    picked = e;
-                    distance = dist2;
-                }
-            } else {
-                final float len = ray.direction.dot(
-                        position.x - ray.origin.x,
-                        position.y - ray.origin.y,
-                        position.z - ray.origin.z);
-
-                if (len < 0f)
-                    continue;
-
-                float dist2 = position.dst2(
-                        ray.origin.x + ray.direction.x * len,
-                        ray.origin.y + ray.direction.y * len,
-                        ray.origin.z + ray.direction.z * len);
-
-                if (distance >= 0f && dist2 > distance)
-                    continue;
-
-                if (dist2 <= mc.boundingRadius * mc.boundingRadius) {
-                    picked = e;
-                    distance = dist2;
-                }
-            } // if ....
-            /*            Gdx.app.log("asdf", String.format("mc.id=%d, dx = %f, pos=(%f,%f,%f)",
-                    mc.id, distance, position.x, position.y, position.z ));*/
+            float tmpDistance = pickRay(e.getComponent(ModelComponent.class), ray, distance);
+            if (tmpDistance >= 0) {
+                picked = e;
+                distance = tmpDistance;
+            }
         }
 
         if (null != picked) {
@@ -117,10 +84,37 @@ public class PickRaySystem extends IteratingSystem implements EntityListener {
     }
 
 
+    public float pickRay(ModelComponent mc, Ray ray, float distance) {
+
+        float tmpDistance = -1;
+        mc.modelInst.transform.getTranslation(position).add(mc.center);
+
+        final float len = ray.direction.dot(
+                position.x - ray.origin.x,
+                position.y - ray.origin.y,
+                position.z - ray.origin.z);
+
+        if (len >= 0f) {
+
+            float dist2 = position.dst2(
+                    ray.origin.x + ray.direction.x * len,
+                    ray.origin.y + ray.direction.y * len,
+                    ray.origin.z + ray.direction.z * len);
+
+            if (distance >= 0f && dist2 > distance) {
+                ; // item is not selected if distance is farther than reference distance
+
+            } else if (dist2 <= mc.boundingRadius * mc.boundingRadius) {
+                tmpDistance = dist2;
+            }
+        }
+        return tmpDistance;
+    }
+
+
     @Override
     public void entityAdded (Entity entity){
-
-//        addPickObject(entity) ;
+        // empty
     }
 
     @Override
