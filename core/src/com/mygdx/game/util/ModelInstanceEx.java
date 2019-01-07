e2b322718a1043370888996c28ab39d1577c44d4
GlennN <GlennN@anywhere.com>
2019-01-07 12:43:23 -0500

    static void setColorAttribute(ModelInstance inst, Color color)

--- core/src/com/mygdx/game/util/ModelInstanceEx.java
+++ core/src/com/mygdx/game/util/ModelInstanceEx.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.util;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g3d.Material;
@@ -29,12 +30,12 @@ public class ModelInstanceEx extends ModelInstance {
     }
 
 
-    public void setColorAttribute(Color c, float alpha) {
+    public static void setColorAttribute(ModelInstance inst, Color color) {
 
-        setColorAttribute(this, c, alpha);
+        setColorAttribute(inst, color, color.a);
     }
 
-    public static void setColorAttribute(ModelInstance inst, Color c, float alpha) {
+    public static void setColorAttribute(ModelInstance inst, Color color, float alpha) {
 
         /*        ColorAttribute ca = (ColorAttribute) mat.get(ColorAttribute.Diffuse);         */
 
@@ -43,10 +44,11 @@ public class ModelInstanceEx extends ModelInstance {
 //        if (null == mat)
 //            return; // throw new GdxRuntimeException("not found");
 
-        mat.set(ColorAttribute.createDiffuse(c));
+        mat.set(ColorAttribute.createDiffuse(color));
 
         BlendingAttribute blendingAttribute =
                 new BlendingAttribute(true, GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA, alpha);
+
         mat.set(blendingAttribute);
     }
 
@@ -110,13 +112,18 @@ mat.remove(BlendingAttribute.Type);
         ModelInstance instance = new ModelInstance(model, transform, node);
         Node modelNode = instance.getNode(node);
 
-// https://xoppa.github.io/blog/loading-a-scene-with-libgdx/
-        instance.transform.set(modelNode.globalTransform);
-        modelNode.translation.set(0, 0, 0);
-        modelNode.scale.set(1, 1, 1);
-        modelNode.rotation.idt();
-        instance.calculateTransforms();
+        if (null == modelNode){
+            Gdx.app.log("ModelInstanceEx", "Failed, inst.nodes.size = " + instance.nodes.size);
+            instance = null;
 
+        } else {
+// https://xoppa.github.io/blog/loading-a-scene-with-libgdx/
+            instance.transform.set(modelNode.globalTransform);
+            modelNode.translation.set(0, 0, 0);
+            modelNode.scale.set(1, 1, 1);
+            modelNode.rotation.idt();
+            instance.calculateTransforms();
+        }
         return instance;
     }
 }
