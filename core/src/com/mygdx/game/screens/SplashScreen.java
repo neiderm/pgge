840b6f8eb99608f9497c7cf5ae10bdba8664621e
neiderm <ded.reckning@gmail.com>
2019-01-12 13:13:02 -0500

    Input.Buttons.LEFT as "Select" function (location of PS1 Cross button)

--- core/src/com/mygdx/game/screens/SplashScreen.java
+++ core/src/com/mygdx/game/screens/SplashScreen.java
@@ -3,10 +3,12 @@ package com.mygdx.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.mygdx.game.characters.InputStruct;
 
 /**
  * Created by utf1247 on 7/16/2018.
@@ -19,7 +21,7 @@ public class SplashScreen implements Screen {
     private static String dataFileName = "SelectScreen.json";
     private SpriteBatch batch;
     private Texture ttrSplash;
-
+    private Controller connectedCtrl;
 
     SplashScreen() {
         batch = new SpriteBatch();
@@ -27,6 +29,8 @@ public class SplashScreen implements Screen {
 
         // not using a listener right now ... make sure we haven't left a stale "unattended" input processor lying around!
         Gdx.input.setInputProcessor(new Stage());
+
+        connectedCtrl = InputStruct.getConnectedCtrl(0);
     }
 
     @Override
@@ -47,13 +51,36 @@ public class SplashScreen implements Screen {
 
         // simple polling for a tap on the touch screen
         if (isTouched
-                || Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+                || Gdx.input.isKeyPressed(Input.Keys.SPACE)
+                || connectedCtrl.getButton(Input.Buttons.LEFT))  // "X Button "
+        {
 
             GameWorld.getInstance().showScreen(new LoadingScreen(dataFileName, false, LoadingScreen.ScreenTypes.SETUP));
 //            Gdx.input.setCatchBackKey(true);
 
             Gdx.app.log("Splash Screen", "-> GameWorld.getInstance().showScreen(new LoadingScreen");
         }
+
+        // this is here just to showt he mapping on my old USB controller
+        // "Triangle"
+        if (connectedCtrl.getButton(Input.Buttons.BACK))
+         Gdx.app.log("SplashScreen", "controlller button  BACK" );
+
+        // Upper Left "shoulder" button ????
+        if (connectedCtrl.getButton(Input.Buttons.FORWARD))
+            Gdx.app.log("SplashScreen", "controlller button  FORW" );
+
+        // "X"
+        if (connectedCtrl.getButton(Input.Buttons.LEFT))
+            Gdx.app.log("SplashScreen", "controlller button  LEFT" );
+
+        // "Circle"
+        if (connectedCtrl.getButton(Input.Buttons.RIGHT))
+            Gdx.app.log("SplashScreen", "controlller button  RIGHT" );
+
+        // "Square"
+        if (connectedCtrl.getButton(Input.Buttons.MIDDLE))
+            Gdx.app.log("SplashScreen", "controlller button  MIDDL" );
     }
 
     @Override
