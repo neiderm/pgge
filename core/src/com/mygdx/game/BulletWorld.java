1a8a6552f4b06b50739aaacb350704bef2b103b1
neiderm <ded.reckning@gmail.com>
2019-03-14 18:01:26 -0400

    USE_DDBUG_DRAW

--- core/src/com/mygdx/game/BulletWorld.java
+++ core/src/com/mygdx/game/BulletWorld.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2019 Glenn Neidermeier
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
@@ -19,14 +35,13 @@ import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody;
 import com.badlogic.gdx.physics.bullet.dynamics.btSequentialImpulseConstraintSolver;
 import com.badlogic.gdx.physics.bullet.linearmath.btIDebugDraw;
 import com.badlogic.gdx.utils.Disposable;
-//import com.badlogic.gdx.utils.GdxRuntimeException;
 
 
 public class BulletWorld implements Disposable {
 
     private static BulletWorld instance = null;
 
-    private static final boolean USE_DDBUG_DRAW = false;
+    public static  boolean USE_DDBUG_DRAW = false;
     private DebugDrawer debugDrawer;
     private btCollisionConfiguration collisionConfiguration;
     private btCollisionDispatcher dispatcher;
