bb02d9bbb6e834a863a560a6be1e4d6cff2bffdd
GlennN <GlennN@anywhere.com>
2019-06-21 06:41:25 -0400

    Tracker "exponential filter" (improve camera tracking)

--- core/src/com/mygdx/game/controllers/TrackerSB.java
+++ core/src/com/mygdx/game/controllers/TrackerSB.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2019 Glenn Neidermeier
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.mygdx.game.controllers;
 
 import com.badlogic.gdx.ai.steer.Limiter;
@@ -92,13 +107,16 @@ public class TrackerSB<T extends Vector<T>> extends SteeringBehavior<T> {
         adjTgtPosition.z += dZ * dist;
 
         // e = setpoint - process .... process is current value of whatever we're controlling
+        Vector3 prevError = error;
         error = adjTgtPosition.sub(translation);
+        // apply exponential filter (helps with "hunting" but probably still needs some limitation on the output)
+        error.add(prevError).scl(0.5f); // error = (error + prevError) / 2;
 
 //        float kI = 0.001f;
 //        integral.add(error.cpy().scl(delta * kI));
 
 //        float kP = 0.1f;
-        output.set(error.cpy().scl(kP)); // proportional
+        output.set(error.scl(kP)); // proportional
         translation.add(output);
 
         currentPositionTransform.setTranslation(translation);
