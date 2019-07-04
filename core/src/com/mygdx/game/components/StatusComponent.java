452b45965e439c0728d990e52a352779dd3bad89
neiderm <ded.reckning@gmail.com>
2019-07-03 20:17:34 -0400

    default count so things don't disappear

--- core/src/com/mygdx/game/components/StatusComponent.java
+++ core/src/com/mygdx/game/components/StatusComponent.java
@@ -11,14 +11,12 @@ public class StatusComponent implements Component {
 
     public String name;
 
-    public final static int  FPS = 60;
-    public final static int LIFECLOCKDEFAULT = 7 * FPS;
-    public final static int DIECLOCKDEFAULT = 10 * FPS;
+    private static final int FPS = 60;
+    private static final int LIFECLOCKDEFAULT = 999 * FPS;
+    private static final int DIECLOCKDEFAULT = 10 * FPS;
 
     public int lifeClock = LIFECLOCKDEFAULT;
     public int dieClock = DIECLOCKDEFAULT;
-
-    public int hitCount;
 
     // hackme: all should be removeable EXCEPT player
     public boolean isEntityRemoveable = true;
