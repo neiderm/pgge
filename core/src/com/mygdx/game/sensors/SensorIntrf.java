aa3cca89e2aecd0723a3e66df71886aac58c05cc
neiderm <ded.reckning@gmail.com>
2019-07-08 17:07:51 -0400

    SensorIntrf (forgot on last commit)

---
+++ core/src/com/mygdx/game/sensors/SensorIntrf.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.sensors;
+
+import com.badlogic.ashley.core.Entity;
+import com.mygdx.game.util.IStatusUpdater;
+
+/**
+ * Created by neiderm on 7/5/2018.
+ */
+
+public abstract class SensorIntrf implements IStatusUpdater /* tmp */ {
+
+    public void update(Entity e){/*mt*/}
+}
