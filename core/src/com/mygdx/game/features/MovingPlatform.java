476d3174b0e2db8eb0d9c4753b13b22acdf5655e
neiderm <ded.reckning@gmail.com>
2019-08-05 16:19:02 -0400

    3dee moving platformz

--- core/src/com/mygdx/game/features/MovingPlatform.java
+++ core/src/com/mygdx/game/features/MovingPlatform.java
@@ -26,74 +26,94 @@ import com.mygdx.game.components.ModelComponent;
 
 public class MovingPlatform extends FeatureAdaptor {
     /*
-     * simle platform moves from point a to point b.
+     * simle platform moves directly from point A to point B using simple proportional control
      * non-simple platform would follow some kind of planned pattern
      */
     public MovingPlatform() { // mt
     }
 
-    /* these shall be Vector3 so that the endpoints are simple A to B to A in 3D space and don't have to be on same plne */
-//    private static final float Z_MIN = -5f;
-//    private static final float Z_MAX = 15.0f;
+    private final float K_PROP = 0.04f; // 2b loaded from JSON
 
-    private static final float TRAVEL_STEP = 0.075f;
-
+    private static final float RAMP_STEPS = 20; // whatever
     private static final int STOP_TIME = 3 * 60; // FPS
+
     private int stopTimer = STOP_TIME;
+    private int nStepCnt;
 
-    private static final float STEP_RAMP_INC = 0.005f;
-    private float increment = STEP_RAMP_INC;
-
-
-    private int travelDirection = 1;
+    private Vector3 vA;// = new Vector3();
+    private Vector3 vB;// = new Vector3();
+    private Vector3 vStep = new Vector3();
+    private Vector3 vError = new Vector3();
     private Vector3 tmpV = new Vector3();
 
-    // we travel point A to point B
-    private Vector3 vA = new Vector3();
-    private Vector3 vB = new Vector3();
 
     @Override
     public void update(Entity featureEnt) {
         //               super.update(featureIntrf);
         ModelInstance instance = featureEnt.getComponent(ModelComponent.class).modelInst;
 
+        if (null == vA || null == vB){
+            // bah grab the starting Origin (translation) of the entity from the instance data because we don't have proper init/constructions
+            vA = new Vector3(vT0);
+            vB = new Vector3(vT);
+        }
 
         if (stopTimer-- <= 0) {
 
-            // sum of pointA and our offset vector which is by convention in vT
-            vA.set(vT0);  // grab the starting Origin (translation) of the entity from the instance data
-            vB.set(vA);
-            vB.add(vT);
+            instance.transform.getTranslation(tmpV);
+            vError.set(vB).sub(tmpV);
 
-            // see about dat
-            float Z_MIN = vA.z;
-            float Z_MAX = vB.z;
+            tmpV.set(Vector3.Zero);
+            float dst2Error = tmpV.dst2(vError);
+
+            // vA, vB fixed so this part would not change ... get the step vector (SAME DIRECTION as [ vB - vA ]
+            // and scale by kProp
+            vStep.set(vB);
+            vStep.sub(vA);
+
+            // take unit vector of (vB - vA)
+            tmpV.set(Vector3.Zero);
+            float dst2Step = tmpV.dst2(vStep);
+            tmpV.set(vStep);
+            tmpV.scl(1/(float)Math.sqrt(dst2Step));
 
 
-            if (increment < TRAVEL_STEP) {
-                increment += STEP_RAMP_INC;
+            // apply kProp as gain on unit vector for constant speed (after the ramp) ... NOT on the
+            // Error term ... so it's not really prop control!
+            vStep.set(tmpV.scl(K_PROP));
+
+
+            // scale vStep to ramping increment * ramp_count
+            vStep.scl(1/RAMP_STEPS); // would be final/const .. using the dst2 value below
+
+            // actual_step = ramp_inc_mag * number_of_steps_cnt
+            if (nStepCnt < RAMP_STEPS) {
+                nStepCnt += 1;
+            }
+            vStep.scl(nStepCnt);
+
+            tmpV.set(Vector3.Zero);
+            float dst2StepRampIncV = tmpV.dst2(vStep); // use this to test proximity to point B
+
+
+            // use proportional output but capped at whatever the ramped value is
+            tmpV.set(vError.scl(( K_PROP * 0.5f) ));
+            if (tmpV.x <= vStep.x && tmpV.y <= vStep.y && tmpV.z <= vStep.z){
+//                vStep.set(tmpV);
             }
 
-            float finalIncremnt = increment * travelDirection;
+            instance.transform.trn(vStep); // only moves visual model, not the body!
 
-            instance.transform.trn(0, 0, finalIncremnt); // only moves visual model, not the body!
+            if ( dst2Error < dst2StepRampIncV ) {
 
-            tmpV = instance.transform.getTranslation(tmpV);
-            float newZ = tmpV.z;
-
-            if (newZ >= Z_MAX || newZ <= Z_MIN) {
-                travelDirection *= -1;
                 stopTimer = STOP_TIME;
-                increment = STEP_RAMP_INC;
+                nStepCnt = 0;
 
-                // snap in the destination coordinate in case of overshooot
-                if (newZ > Z_MAX)
-                    tmpV.z = Z_MAX;
-
-                if (newZ < Z_MIN)
-                    tmpV.z = Z_MIN;
-
-                instance.transform.setTranslation(tmpV);
+                //  we are there. swap end points
+                tmpV.set(vA);
+                vA.set(vB);
+                vB.set(tmpV);
+                instance.transform.setTranslation(vA); // snap-to
             }
 
             BulletComponent bc = featureEnt.getComponent(BulletComponent.class);
