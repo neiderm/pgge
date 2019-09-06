0982e347ce4965c10be993274528f8c8b16e033d
neiderm <neiderm@neiderm>
2019-09-06 15:15:09 -0400

    mkStaticFromDynamicEntity

--- core/src/com/mygdx/game/components/CompCommon.java
+++ core/src/com/mygdx/game/components/CompCommon.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright (c) 2019 Glenn Neidermeier
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mygdx.game.components;
 
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
 import com.mygdx.game.sceneLoader.GameObject;
+import com.mygdx.game.sceneLoader.InstanceData;
 
 /*
  * the catch-all of the moment ...
@@ -13,7 +32,7 @@ public class CompCommon {
     CompCommon(){ // mt
     }
 
-    public static void explode(Engine engine, Entity picked){
+    public static void explode(Engine engine, Entity picked /* , ModelComponent mc */){
 
         ModelComponent mc = picked.getComponent(ModelComponent.class);
         Vector3 translation = new Vector3();
@@ -32,5 +51,39 @@ public class CompCommon {
 
         // mark dead entity for deletion
         picked.add(new StatusComponent(true));
+    }
+
+    public static void mkStaticFromDynamicEntity(Entity sss) {
+
+        BulletComponent bc = sss.getComponent(BulletComponent.class);
+        if (null == bc) {
+            Gdx.app.log("collisionHdlr", "BulletComponent bc =  === NULLLL");
+            return; // bah processing object that should already be "at rest" ???? .....................................................
+        }
+
+        bc.body.setCollisionFlags( bc.body.getCollisionFlags() & ~btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK);
+
+        // insert a newly created game OBject into the "spawning" model group
+        GameObject gameObject = new GameObject();
+        gameObject.isShadowed = true;
+
+        Vector3 size = new Vector3(0.5f, 0.5f, 0.5f); /// size of the "box" in json .... irhnfi  bah
+        gameObject.scale = new Vector3(size);
+
+        gameObject.objectName = "box";
+
+        Vector3 translation = new Vector3();
+//                translation = bc.body.getWorldTransform().getTranslation(translation);
+
+        ModelComponent mc = sss.getComponent(ModelComponent.class);
+        Matrix4 tmpM4 = mc.modelInst.transform;
+//            translation = tmpM4.getTranslation(translation);
+        gameObject.getInstanceData().add(new InstanceData(tmpM4.getTranslation(translation)));
+
+//                GameWorld.getInstance().addSpawner(gameObject);         ///    toooodllly dooodddd    object is added "kinematic" ???
+
+        StatusComponent sc = new StatusComponent();
+        sc.deleteFlag = 2;         // flag bullet Comp for deletion
+        sss.add(sc);
     }
 }
--- core/src/com/mygdx/game/features/KillThing.java
+++ core/src/com/mygdx/game/features/KillThing.java
@@ -2,14 +2,7 @@ package com.mygdx.game.features;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
-import com.mygdx.game.components.BulletComponent;
-import com.mygdx.game.components.ModelComponent;
-import com.mygdx.game.components.StatusComponent;
-import com.mygdx.game.sceneLoader.GameObject;
-import com.mygdx.game.sceneLoader.InstanceData;
+import com.mygdx.game.components.CompCommon;
 
 /*
  * ....Feature  "DroppedBody" ..  .......... becomes un-dynamic
@@ -27,49 +20,19 @@ public class KillThing extends KillSensor {
         if (contactCount > 0) {
 
             if (contactBucket > 0) { // each update, either empty 1 drop from the bucket and return, or if bucket empty then process it
+
                 contactBucket -= 1;
+
             } else {
+
                 if (0 == contactBucket) {
+
                     Gdx.app.log(" asdfdfd", "object is at rest ?? ");
                     contactCount = 0;
-                    collisionHdlr(sensor);
+                    CompCommon.mkStaticFromDynamicEntity(sensor);
                 }
             }
         }
-    }
-
-    private void collisionHdlr(Entity sss) {
-
-        BulletComponent bc = sss.getComponent(BulletComponent.class);
-        if (null == bc) {
-            Gdx.app.log("collisionHdlr", "BulletComponent bc =  === NULLLL");
-            return; // bah processing object that should already be "at rest" ???? .....................................................
-        }
-
-        bc.body.setCollisionFlags( bc.body.getCollisionFlags() & ~btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK);
-
-        // insert a newly created game OBject into the "spawning" model group
-        GameObject gameObject = new GameObject();
-        gameObject.isShadowed = true;
-
-        Vector3 size = new Vector3(0.5f, 0.5f, 0.5f); /// size of the "box" in json .... irhnfi  bah
-        gameObject.scale = new Vector3(size);
-
-        gameObject.objectName = "box";
-
-        Vector3 translation = new Vector3();
-//                translation = bc.body.getWorldTransform().getTranslation(translation);
-
-        ModelComponent mc = sss.getComponent(ModelComponent.class);
-        Matrix4 tmpM4 = mc.modelInst.transform;
-//            translation = tmpM4.getTranslation(translation);
-        gameObject.getInstanceData().add(new InstanceData(tmpM4.getTranslation(translation)));
-
-//                GameWorld.getInstance().addSpawner(gameObject);         ///    toooodllly dooodddd    object is added "kinematic" ???
-
-        StatusComponent sc = new StatusComponent();
-        sc.deleteFlag = 2;         // flag bullet Comp for deletion
-        sss.add(sc);
     }
 
     @Override
