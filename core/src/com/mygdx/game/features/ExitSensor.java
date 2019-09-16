8e7219b8fed776623268333d6cfb34f3d1cba7ac
bones <ded.reckning@gmail.com>
2019-09-15 20:00:42 -0400

    color exit sensor

--- core/src/com/mygdx/game/features/ExitSensor.java
+++ core/src/com/mygdx/game/features/ExitSensor.java
@@ -1,7 +1,12 @@
 package com.mygdx.game.features;
 
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.mygdx.game.components.ModelComponent;
 import com.mygdx.game.components.StatusComponent;
+import com.mygdx.game.util.ModelInstanceEx;
 
 public class ExitSensor extends OmniSensor {
 
@@ -12,12 +17,18 @@ public class ExitSensor extends OmniSensor {
 
         if (isTriggered){
 
+            ModelComponent mc = sensor.getComponent(ModelComponent.class);
+            ModelInstance mi = mc.modelInst;
+            ModelInstanceEx.setColorAttribute(mi, new Color(Color.FIREBRICK)); // tmp
+
             StatusComponent sc = target.getComponent(StatusComponent.class);
 
-            if (null != sc){
+            if (null != sc.UI){
 
                 sc.UI.canExit = true;
             }
+            else
+                Gdx.app.log("barf", "gacked");
         }
     }
 }
