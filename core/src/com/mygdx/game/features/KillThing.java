282c1d07859e00651cc60f8de898eced328108ac
neiderm <neiderm@neiderm>
2019-09-17 15:21:10 -0400

    It may be a base for bomb, grenade, mine whatever.

--- core/src/com/mygdx/game/features/KillThing.java
+++ core/src/com/mygdx/game/features/KillThing.java
@@ -1,69 +1,32 @@
 package com.mygdx.game.features;
 
 import com.badlogic.ashley.core.Entity;
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.mygdx.game.components.CompCommon;
+import com.mygdx.game.components.ModelComponent;
+import com.mygdx.game.util.ModelInstanceEx;
 
 /*
  * ....Feature  "DroppedBody" ..  .......... becomes un-dynamic
+ * Presently, this one  strips off the physics body, it does NOT respawn as a new entity.
  */
 public class KillThing extends KillSensor {
 
-    private int contactCount;
-    private int contactBucket;
-
-    @Override
-    public void update(Entity sensor) {
-
-        super.update(sensor);
-
-        if (contactCount > 0) {
-
-            if (contactBucket > 0) { // each update, either empty 1 drop from the bucket and return, or if bucket empty then process it
-
-                contactBucket -= 1;
-
-            } else {
-
-                if (0 == contactBucket) {
-
-                    Gdx.app.log(" asdfdfd", "object is at rest ?? ");
-                    contactCount = 0;
-                    CompCommon.physicsBodyMarkForRemoval(sensor);
-                }
-            }
-        }
-    }
-
-    /*
-     * have to put a "collision processor" in json
-     */
     @Override
     public void onProcessedCollision(Entity ee){
 
-        super.onProcessedCollision(ee);
-//        CompCommon.entityRmvPhysicsBody(ee); // entityRmvPhysicsBody
-    }
+        // toooooooooodooo ... can it have option when to activate or detonate (bomb vs. mine?)
 
-    @Override
-    public void onCollision(Entity myCollisionObject, int id) {
+//        super.onProcessedCollision(ee);
+        CompCommon.physicsBodyMarkForRemoval(ee);
 
-        Gdx.app.log("onCollision", "int = " + id);
-
-        contactCount += 1; // always increment (zero'd out when bucket is emptied)
-
-        // bucket fills faster to ensure that it takes time to empty the bucket once the contacts have rung out ....
-        contactBucket = 60; // idfk ... allow at least 1 frames worth of updates to ensure object is at rest?
-
-/* test ..
-        BulletComponent bc = myCollisionObject.getComponent(BulletComponent.class);
-        bc.body.setCollisionFlags(bc.body.getCollisionFlags() & ~btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK);
-*/
-
-//        int as = bc.body.getActivationState();
-//        Gdx.app.log("asdf", "activation state = " + as);
-
-//        m4 = bc.body.getWorldTransform();
-//        translationASDFG = m4.getTranslation(translationASDFG);
+        ModelComponent mc = ee.getComponent(ModelComponent.class);
+        ModelInstance mi = mc.modelInst;
+        if (mi.materials.size > 0) {
+            ModelInstanceEx.setColorAttribute(mi, new Color(Color.RED), 0.9f); //tmp?
+        }
+//        else
+//            Gdx.app.log("asdf", "asdf");
     }
 }
