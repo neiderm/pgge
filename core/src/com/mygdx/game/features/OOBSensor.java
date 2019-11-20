6848f213f224ee7f75448d5f6673b6a9f07097cb
neiderm <neiderm@neiderm>
2019-11-20 14:35:00 -0500

    Separated OOB from KillSensor for now

--- core/src/com/mygdx/game/features/OOBSensor.java
+++ core/src/com/mygdx/game/features/OOBSensor.java
@@ -1,19 +1,31 @@
 package com.mygdx.game.features;
 
+import com.badlogic.ashley.core.Entity;
+import com.mygdx.game.components.StatusComponent;
+
 /**
  * Created by neiderm on 8/24/2019.
  * <p>
- * Out-of-bounds sensor is simply an inverted kill sensor
+ * Out-of-bounds sensor is simply an inverted kill sensor ...
+ *  or was ..... tmp ?  possibly re-integrate as sub-class of KS.
  */
-
-public class OOBSensor extends KillSensor {
+public class OOBSensor /*extends KillSensor*/ extends OmniSensor {
 
     @Override
-
     public void init(Object obj){
 
         super.init(obj);
 
         inverted = true;
+    }
+
+    @Override
+    public void update(Entity sensor) {
+
+        super.update(sensor);
+
+        if (isTriggered) {
+            target.getComponent(StatusComponent.class).lifeClock = 0;
+        }
     }
 }
