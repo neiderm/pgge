d43e8e6e03aebd682d38467632eca623ba53e7c0
neiderm <ded.reckning@gmail.com>
2020-02-03 17:13:02 -0500

    OOB Sensor explicitly set lifeclock 0

--- core/src/com/mygdx/game/features/OOBSensor.java
+++ core/src/com/mygdx/game/features/OOBSensor.java
@@ -1,5 +1,8 @@
 package com.mygdx.game.features;
 
+import com.badlogic.ashley.core.Entity;
+import com.mygdx.game.components.StatusComponent;
+
 /**
  * Created by neiderm on 8/24/2019.
  * <p>
@@ -15,14 +18,14 @@ public class OOBSensor extends OmniSensor {
 
         inverted = true; // instead of requiing this flag, it could just get by by doing the subclass
     }
-//
-//    @Override
-//    public void update(Entity sensor) {
-//
-//        super.update(sensor);
-//
-//        if (isTriggered) {
-//            target.getComponent(StatusComponent.class).lifeClock = 0;
-//        }
-//    }
+
+    @Override
+    public void update(Entity sensor) {
+
+        super.update(sensor);
+
+        if (isTriggered) {
+            target.getComponent(StatusComponent.class).lifeClock = 0;
+        }
+    }
 }
