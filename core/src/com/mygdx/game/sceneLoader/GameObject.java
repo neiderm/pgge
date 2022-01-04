ca4f2b4f580bb39aa51cf73dfdd5de00405d8889
neidern <ded.reckning@gmail.com>
2022-01-04 16:31:13 -0500

    public GameObject(Vector3 translation, Quaternion rotation)

--- core/src/com/mygdx/game/sceneLoader/GameObject.java
+++ core/src/com/mygdx/game/sceneLoader/GameObject.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Glenn Neidermeier
+ * Copyright (c) 2021-2022 Glenn Neidermeier
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.model.Node;
+import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.math.collision.BoundingBox;
 import com.badlogic.gdx.physics.bullet.collision.CollisionConstants;
@@ -48,9 +49,10 @@ public class GameObject {
         this.isShootable = false;
     }
 
-    public GameObject(float mass) {
+    public GameObject(Vector3 translation, Quaternion rotation) {
         this();
-        this.mass = mass;
+        this.mass = 1.0f;
+        this.getInstanceData().add(new InstanceData(translation, rotation));
     }
 
     public GameObject(String objectName) {
--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -519,11 +519,11 @@ public class GameScreen extends BaseScreenWithAssetsEngine {
                         }
                     }
                 }
-                engine.removeEntity(e); // calls BulletSystem:entityRemoved(), but the bc is not useable :(
+                engine.removeEntity(e); // physics comp disposals in BulletSystem:entityRemoved()
             } else {
                 if (2 == sc.deleteFlag) { // will use flags for comps to remove
                     // only the BC is removed, but the entity is not destroyed
-                    e.remove(BulletComponent.class); // triggers BulletSystem:entityRemoved() ?????
+                    e.remove(BulletComponent.class);  // physics comp disposals in BulletSystem:entityRemoved()
                 }
             }
             sc.deleteFlag = 0;
@@ -546,16 +546,9 @@ public class GameScreen extends BaseScreenWithAssetsEngine {
         if ((null != shape) && shape.className.equals("btCompoundShape")) {
             Vector3 translation = new Vector3();
             Quaternion rotation = new Quaternion();
-
-            /*
-             *todo cosolidate game object instantiation and instance
-             */
-            GameObject gameObject = new GameObject(1);
-
-            gameObject.getInstanceData().add(new InstanceData(
+            GameObject gameObject = new GameObject(
                     modelInst.transform.getTranslation(translation),
-                    modelInst.transform.getRotation(rotation))
-            );
+                    modelInst.transform.getRotation(rotation));
             // build nodes by iterating the node id list, which hopefully is in same index order as when the comp shape was builtup
             gameObject.buildChildNodes(engine, modelInst.model, (btCompoundShape) shape);
         } else {
